d54a424721ed5f9bb60c4616e7f470d89d468d30
Rahul <rahulpathak263@gmail.com>
2020-11-03 13:11:41 +0530

    Update cvd19.py

--- healthchecker/cvd19.py
+++ healthchecker/cvd19.py
@@ -202,10 +202,6 @@ app.layout = html.Div(style={'textAlign': 'Center'},  children=[
 
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:8050/')
-
-
-
-
 if __name__ == '__main__':
     Timer(1, open_browser).start();
     app.run_server(debug=True)
